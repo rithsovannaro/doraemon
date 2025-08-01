f04e6a6348c88bf471e7e779c213a9137ea098b0
rithsovannaro <rithsovannaro7@gmail.com>
2025-08-02 00:22:40 +0700

    Remove build and CMake-generated files from repo

--- build/_deps/xlnt-build/source/third-party/libstudxml/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Admin/Desktop/githubclone/Dore/build/_deps/xlnt-src/third-party/libstudxml.build
-# Build directory: C:/Users/Admin/Desktop/githubclone/Dore/build/_deps/xlnt-build/source/third-party/libstudxml
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
